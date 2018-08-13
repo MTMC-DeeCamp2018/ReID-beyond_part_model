b01dac3916f6ce594fa912185f50e72749feb94e
luzhoutao02@gmail.com <luzhoutao02@gmail.com>
2018-08-13 20:10:34 +0800

    save features

--- script/run.py
+++ script/run.py
@@ -25,7 +25,7 @@ from bpm.utils.utils import set_devices
 # Arguments #
 #############
 
-sys_device_ids = (0,)
+sys_device_ids = ()
 TVT, TMO = set_devices(sys_device_ids)
 
 # image input
@@ -41,7 +41,7 @@ num_stripes = 6
 local_conv_out_channels = 256
 
 # weight file
-model_weight_file = "/mnt/md1/lztao/deecamp/ReID-beyond_part_model/baseline_log/ckpt.pth"
+model_weight_file = "/Users/luzhoutao/study_abroad/7-DeeCamp/workspace/beyond-part-models/model_weights/ckpt.pth" #"/mnt/md1/lztao/deecamp/ReID-beyond_part_model/baseline_log/ckpt.pth"
 
 
 
@@ -104,18 +104,66 @@ def start():
 
 def run(preprocessor, extractor, im_path):
     im = np.asarray(Image.open(im_path))
+    print(im.shape)
     # preprocessing
     im, _ = preprocessor(im)
+    print(im.shape)
     im = np.stack([im], axis=0)
     return extractor(im)
 
+def run_video(preprocessor, extractor, video_path, bbox_path):
+    # read video
+    cap = cv2.VideoCapture(video_path)
+    fid, frame = 0, None
+
+    records,count = [], 0
+
+    with open(bbox_path, 'r') as f:
+        line = f.readline()
+        while line:
+            id, x, y, w, h, conf = line.strip().split()
+            id, x, y, w, h, conf = int(id), float(x), float(y), float(w), float(h), float(conf)
+            x1, y1 = int(np.round(max(0, x))), int(np.round(max(0, y)))
+            x2, y2 = int(np.round(max(0, x + w))), int(np.round(max(0, y + h)))
+
+            while fid < id:
+                succ, frame = cap.read()
+                fid += 1
+
+            im = frame[y1:y2, x1:x2]
+            im, _ = preprocessor(im)
+            im = np.stack([im], axis=0)
+
+            feat = extractor(im)
+
+            rec = np.zeros([6 + feat.shape[1]])
+            rec[0], rec[1], rec[2], rec[3], rec[4], rec[5] = id, x, y, w, h, conf
+            rec[6:] = feat[0]
+
+            records.append(rec)
+
+            count += 1
+            line = f.readline()
+            print(str(count), end='\r')
+
+    records = np.stack(records, axis=0)
+    return records
 
 if __name__ == '__main__':
-    im_dir = '/mnt/md1/lztao/deecamp/ReID-beyond_part_model/eval/people/s2c0/'
+    '''im_dir = '../eval/people/s2c0/0' #'/mnt/md1/lztao/deecamp/ReID-beyond_part_model/eval/people/s2c0/'
     feat_dir = '/mnt/md1/lztao/deecamp/ReID-beyond_part_model/eval/feats/s2c0'
 
     preprocessor, extractor = start()
     for im_name in os.listdir(im_dir):
         feat = run(preprocessor, extractor, os.path.join(im_dir, im_name))
+        input()
         np.save(os.path.join(feat_dir, im_name.split('.')[0]), feat)
-        print(im_name, end='\r')
+        print(im_name, end='\r')'''
+
+    bbox_path = "/Users/luzhoutao/study_abroad/7-DeeCamp/workspace/Dataset/epfl/bboxes_yolo/bbox_1_c0.txt"
+    video_path = "/Users/luzhoutao/study_abroad/7-DeeCamp/workspace/Dataset/epfl/terrance_seq/terrace1-c0.avi"
+    bbox_feat_path = "/Users/luzhoutao/study_abroad/7-DeeCamp/workspace/Dataset/epfl/bbox_feats_yolo/bbox_feat_1_c0.npy"
+
+    preprocessor, extractor = start()
+    bbox_feat = run_video(preprocessor, extractor, video_path, bbox_path)
+    np.save(bbox_feat_path, bbox_feat)
